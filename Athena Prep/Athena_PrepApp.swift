85728b8f008d07d66d07c196bed60abcb7116e3a
Avanti Singh <avantisingh19@gmail.com>
2023-06-13 09:57:51 -0500

    Initial Commit

---
+++ Athena Prep/Athena_PrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Athena_PrepApp.swift
+//  Athena Prep
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Athena_PrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
